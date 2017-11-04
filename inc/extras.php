b7e771e3febd82e864c1607e49ed307cdec465b0
Anne Minford <anneminford@btinternet.com>
2017-11-04 14:47:00 +0000

    How to turn the Read More tag into a Bootstrap button

--- inc/extras.php
+++ inc/extras.php
@@ -87,3 +87,12 @@ function bootstrapwp_setup_author() {
 	}
 }
 add_action( 'wp', 'bootstrapwp_setup_author' );
+
+/**
+ * Custom Read More Button
+ */
+function modify_read_more_link() {
+
+	return '<p><a class="more-link btn btn-default" href="' . get_permalink() . '">Read More</a></p>';
+}
+add_filter( 'the_content_more_link', 'modify_read_more_link' );
