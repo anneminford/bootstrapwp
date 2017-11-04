d821bf22f1b474a2f544365b36a29197a123872d
Anne Minford <anneminford@btinternet.com>
2017-11-04 13:37:43 +0000

    Customizing the WordPress Sidebar

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,9 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <div id="secondary" class="widget-area col-md-3 col-lg-3" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<div class="well">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</div>
 </div><!-- #secondary -->
 
 </div> <!-- .row -->
