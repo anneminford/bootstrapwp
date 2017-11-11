16ff6d856c5bf27f598f6cc50de239573b73fd3d
Anne Minford <anneminford@btinternet.com>
2017-11-11 17:52:06 +0000

    Homepage Sorter – Adding Hero Post

--- includes/home-homecontent.php
+++ includes/home-homecontent.php
@@ -1,10 +1,17 @@
 <?php
 /**
- * File used for homepage hero content module
+ * File used for homepage static page content module
  *
  * @package WordPress
  */
 ?>
 
 
-<h1>Hero Content</h1>
+<?php while( have_posts() ) : the_post(); ?>
+
+<?php if ( has_post_thumbnail()) : ?>
+	<?php the_post_thumbnail(); ?>
+<?php endif; ?>
+<?php the_content(); ?>
+
+<?php endwhile; ?>
